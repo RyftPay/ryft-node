09bfbaa50bd93fe24081bae718dedd315c2ee977
ryft-ci <richard+1@ryftpay.com>
2025-08-04 08:28:07 +0100

    add signature verifier

--- src/types/events/events.ts
+++ src/types/events/events.ts
@@ -1,51 +1,29 @@
 export interface Events {
-    items: Event[];
+  items: Event[]
 }
 
 export interface Event {
-    id: string;
-    eventType: string;
-    data: EventData;
-    endpoints: Endpoint[];
-    accountId?: string | null | undefined;
-    createdTimestamp: number;
-}
-
-export interface EventData {
-    id: string;
-    accountId?: string | null | undefined;
-    paymentTransactionId?: string | null | string;
-    amount?: number | null | undefined;
-    platformFee?: number | null | string;
-    currency?: string | null | undefined;
-    metadata?: Record<string, string> | null | undefined;
-    status?: string | null | undefined;
-    email?: string | null | undefined;
-    firstName?: string | null | undefined;
-    lastName?: string | null | undefined;
-    defaultPaymentMethod?: string | null | undefined;
-    customer?: {
-        id?: string | null | undefined;
-    } | null | undefined;
-    paymentMethod: {
-        tokenizedDetails: PaymentMethodTokenizedDetails;
-    };
-    createdTimestamp: number;
+  id: string
+  eventType: string
+  data: any
+  endpoints: Endpoint[]
+  accountId?: string | null | undefined
+  createdTimestamp: number
 }
 
 export interface PaymentMethodTokenizedDetails {
-    id: string;
-    stored: boolean;
+  id: string
+  stored: boolean
 }
 
 export interface PausePaymentDetail {
-    reason?: string | null | undefined;
-    resumeAtTimestamp?: number | null | undefined;
-    pausedAtTimestamp: number;
+  reason?: string | null | undefined
+  resumeAtTimestamp?: number | null | undefined
+  pausedAtTimestamp: number
 }
 
 export interface Endpoint {
-    webhookId: string;
-    acknowledged: boolean;
-    attempts: number;
+  webhookId: string
+  acknowledged: boolean
+  attempts: number
 }
