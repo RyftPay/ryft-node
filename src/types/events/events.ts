62e1974eac296ad1d96a221b0df32924a367ef4c
ryft-ci <richard+1@ryftpay.com>
2025-08-04 08:36:13 +0100

    disable linter on specific line

--- src/types/events/events.ts
+++ src/types/events/events.ts
@@ -5,6 +5,7 @@ export interface Events {
 export interface Event {
   id: string
   eventType: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   data: any
   endpoints: Endpoint[]
   accountId?: string | null | undefined
