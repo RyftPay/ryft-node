b78e70ff8cb9292866ba808b0cf9e67cde2f8cc2
lorenzoparis-ryft <lorenzo@ryftpay.com>
2026-02-05 12:57:10 +0000

    src/utils/url.ts - applied fix in API base URL (#8)

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -4,7 +4,7 @@ export const determineBaseUrl = (secretKey: string): string | undefined => {
     if (secretKey.startsWith('sk_sandbox'))
         return RYFT_SANDBOX_URL 
 
-    if (secretKey.startsWith('sk_live'))
+    if (secretKey.startsWith('sk_'))
         return RYFT_LIVE_URL;
 
     return undefined
