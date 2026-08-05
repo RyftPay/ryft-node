e38a3ac341d90a2c14a629e3a3096d324e0bde5d
nick-barron <nickb@ryftpay.com>
2026-08-05 17:14:54 +0100

    undo index changes not needed for this PR

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 export { AccountLinksClient } from "./api/accountLinks";
 export { AccountsClient } from "./api/accounts";
 export { ApplePayClient } from "./api/applePay";
-export { BalanceTransactionsClient } from "./api/balanceTransactions";
-export { BalancesClient } from "./api/balances";
 export { ConversionsClient } from "./api/conversions";
 export { CustomersClient } from "./api/customers";
 export { DisputesClient } from "./api/disputes";
@@ -26,108 +24,5 @@ export { Config } from "./types/config";
 export { determineBaseUrl } from "./utils/url";
 export { Ryft } from "./client";
 
-// Shared types
-export * from "./types/address";
-export * from "./types/businessRole";
-export * from "./types/deleted";
-export * from "./types/errors";
-export * from "./types/gender";
-export * from "./types/recurringInterval";
-export * from "./types/recurringPrice";
-export * from "./types/shippingDetails";
-export * from "./types/url";
-
-// Resource types
-export * from "./types/accountLinks/accountLinks";
-export * from "./types/accounts/accounts";
-export * from "./types/accounts/payoutsReq";
-export * from "./types/applePay/applePay";
-export * from "./types/balanceTransactions/balanceTransactions";
-export * from "./types/balances/balances";
 export * from "./types/conversions/conversions";
 export * from "./types/conversions/req";
-export * from "./types/customers/customers";
-export * from "./types/customers/req";
-export * from "./types/disputes/disputes";
-export * from "./types/disputes/req";
-export * from "./types/documents/documentCategory";
-export * from "./types/documents/documentType";
-export * from "./types/documents/req";
-export * from "./types/events/events";
-export * from "./types/files/files";
-export * from "./types/files/req";
-export * from "./types/inPerson/locations";
-export * from "./types/inPerson/locationsReq";
-export * from "./types/inPerson/orders";
-export * from "./types/inPerson/products";
-export * from "./types/inPerson/skus";
-export * from "./types/inPerson/terminals";
-export * from "./types/inPerson/terminalsReq";
-export * from "./types/paymentMethods/paymentMethods";
-export * from "./types/paymentMethods/req";
-export * from "./types/paymentSessions/paymentSessions";
-export * from "./types/paymentSessions/paymentSessionsReq";
-export * from "./types/paymentSessions/paymentSessionsTransactions";
-export * from "./types/paymentSessions/paymentSessionsTransactionsReq";
-export * from "./types/payoutMethods/payoutMethod";
-export * from "./types/payoutMethods/req";
-export * from "./types/payouts/payouts";
-export * from "./types/payouts/req";
-export * from "./types/persons/personVerification";
-export * from "./types/persons/persons";
-export * from "./types/persons/req";
-export * from "./types/persons/verificationError";
-export * from "./types/platformFees/platformFees";
-export * from "./types/subscriptions/subscriptions";
-export * from "./types/transfers/req";
-export * from "./types/transfers/transfers";
-export * from "./types/webhooks/req";
-export * from "./types/webhooks/webhookEventType";
-export * from "./types/webhooks/webhooks";
-
-// Resource-scoped variants of names already claimed above. `export *` would
-// drop these silently, so the resource that owns the name keeps the flat
-// export and each variant is reached through its own namespace. Names unique
-// to a variant module stay flat.
-
-// `AccountDocumentRequest` is owned by ./types/documents/req
-export type {
-  CreateSubAccountRequest,
-  CreateBusinessRequest,
-  AccountSettingsRequest,
-  AccountTermsOfServiceRequest,
-  UpdateSubAccountRequest,
-  UpdateBusinessRequest,
-} from "./types/accounts/accountsReq";
-export type * as SubAccountRequestTypes from "./types/accounts/accountsReq";
-
-// `Payout` and `Payouts` are owned by ./types/payouts/payouts
-export type * as AccountPayoutTypes from "./types/accounts/payouts";
-
-// `PaymentSettings` is owned by ./types/paymentSessions/paymentSessions
-export type {
-  TerminalReceiptPrintingSource,
-  TerminalTransactionSettings,
-  PaymentMethodOptionSettings,
-  FeeAllocationRequestItem,
-  PaymentFeeAllocationSettings,
-  PaymentPlatformSettings,
-} from "./types/inPerson/terminalSettings";
-export type * as TerminalSettingTypes from "./types/inPerson/terminalSettings";
-
-// `PaymentSession`, `PaymentSessions`, `PaymentSettings`, `CustomerDetails`,
-// `OrderDetails`, `RebillingDetail` and `SplitPaymentDetail` are owned by
-// ./types/paymentSessions/paymentSessions
-export type { SessionPaymentMethod } from "./types/subscriptions/paymentSession";
-export type * as SubscriptionPaymentSessionTypes from "./types/subscriptions/paymentSession";
-
-// `PaymentSettingsRequest` is owned by ./types/paymentSessions/paymentSessionsReq
-export type {
-  CreateSubscriptionRequest,
-  UpdateSubscriptionRequest,
-  SubscriptionPriceRequest,
-  SubscriptionUpdatePriceRequest,
-  SubscriptionIntervalRequest,
-  PauseSubscriptionRequest,
-} from "./types/subscriptions/req";
-export type * as SubscriptionRequestTypes from "./types/subscriptions/req";
